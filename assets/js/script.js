16c1ecb428c5d4f1853eab6a414a8361f840dc74
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-06-29 19:28:55 +0100

    Add constant variables

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Variables
+const cardContainer = document.querySelector(".cards-area");
+const card = document.querySelectorAll(".card");
+const controlArea = document.querySelector(".control-area");
+const scoresContainer = document.querySelector(".score-and-attempts-area");
+const instructions = document.querySelector("instructions");
