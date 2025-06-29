d9f6a8c8990db70f77f420b2e8559e61a5b042a6
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-06-29 19:29:23 +0100

    Add let variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,20 @@ const card = document.querySelectorAll(".card");
 const controlArea = document.querySelector(".control-area");
 const scoresContainer = document.querySelector(".score-and-attempts-area");
 const instructions = document.querySelector("instructions");
+
+const cardArray = [
+    './assets/images/image1.jpg',
+    './assets/images/image2.jpg',
+    './assets/images/image3.jpg',
+    './assets/images/image4.jpg',
+    './assets/images/image5.jpg',
+    './assets/images/image6.jpg',
+    './assets/images/image7.jpg',
+    './assets/images/image8.jpg',
+]
+
+console.log(cardArray);
+
+let cardCounts = {};
+let matchedCardCount = 0;
+let attemptsMade = 0;
